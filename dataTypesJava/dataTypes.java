6d059b626a99c77021d571badab4be4cef5eb825
angelDariaux <angeldariaux@gmail.com>
2022-01-29 23:21:04 -0600

    usar VAR para crear tipos de datos inferidos

--- dataTypesJava/dataTypes.java
+++ dataTypesJava/dataTypes.java
@@ -8,10 +8,30 @@ public class dataTypes {
     double nDouble = 123.123434564567; // ? declarar decimales este es usado para mucha PRECISION en el calculo.
     float nFloate = 123.12343456F; // ? tambien declara decimales pero se necesita de la F al final para que lo reconozca como double.
 
+    // ! Aqui el tipo de dato esta siendo inferido, esto solo pasa en java JDK 11.
+
+    var salary = 10000; // ? int
+    // pension es del 3%
+    var pension = salary * 0.03; // ? double
+    var totalSalary = salary - pension; // ? double
+    
+    
+    var employeeName = "Angel Hernandez";
+ 
+ 
+ 
+ 
+ 
     System.out.println(n);
     System.out.println(nLong);
     System.out.println(nDouble);
     System.out.println(nFloate);
+
+    System.out.println(salary);
+    System.out.println(pension);
+    System.out.println(totalSalary);
+
+    System.out.println("Empleado: " + employeeName + ", Salario: " + totalSalary );
 
   }
 }
